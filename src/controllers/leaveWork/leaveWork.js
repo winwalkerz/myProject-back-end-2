982126e99533e7aadaa590266368a6b407096528
Chanamet Inthuprapa <60160220@go.buu.ac.th>
2021-03-01 18:09:19 +0700

    01-03-64

--- src/controllers/leaveWork/leaveWork.js
+++ src/controllers/leaveWork/leaveWork.js
@@ -216,7 +216,7 @@ class LeaveworkController {
           date_end: input.date_end,
           type: input.type,
           id_status_fk: input.id_status_fk,
-          check: input.check,
+          check: 1,
         },
         { methods: "update", patch: true }
       );
